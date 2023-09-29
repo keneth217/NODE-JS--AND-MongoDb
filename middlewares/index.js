814de4770b47c19e816a0e7d516e4b487d07bdc9
keneth217 <kipyegonkeneth03@gmail.com>
2023-09-29 16:02:41 +0300

    middle wares to catch erors

---
+++ middlewares/index.js
@@ -0,0 +1,21 @@
+const ObjectId = require("mongoose").Types.ObjectId;
+const validateDbId =(req,re,next)=>{
+    if (ObjectId.isValid(req.params.id) == false)
+    res.status(400).json({
+      error: `given object id (${req.params.id}) is not valid`
+    });
+    else
+    next()
+}
+const notFound=(req,res)=>{
+    res.status(404).json({
+        error: "no record with given id:" + req.params.id,
+    })
+}
+const errorHandler=(error,req,res,next)=>{
+    res.status(500).json({error})
+
+}
+module.exports={
+    validateDbId,notFound,errorHandler
+}
