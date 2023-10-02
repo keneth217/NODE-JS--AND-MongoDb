ea71d376a980c31acef7b9b2ffe97f5312306deb
keneth217 <kipyegonkeneth03@gmail.com>
2023-10-02 16:24:16 +0300

    add mongo url  env file

--- db.js
+++ db.js
@@ -1,5 +1,14 @@
+// const mongoose= require('mongoose')
+// const url='mongodb+srv://Admin:root@keneth.wkkcmmh.mongodb.net/admin_db?retryWrites=true&w=majority'
+
+
+// mongoose.set('strictQuery',false)
+// module.exports=()=>{
+//   return  mongoose.connect(url);
+
+// }
 const mongoose= require('mongoose')
-const url='mongodb+srv://Admin:root@keneth.wkkcmmh.mongodb.net/admin_db?retryWrites=true&w=majority'
+const url=process.env.URL
 
 
 mongoose.set('strictQuery',false)
